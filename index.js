85f9b8a7ac8181ab7580d4d64369e34d22e4c614
maryamahmed97-dev <maryamahmed61197@gmil.com>
2026-02-04 00:47:24 +0200

    second

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-let monthlyCost = 350;
-let monthsNumbers = +prompt("Enter number of months? ");
-let finalPrice = monthlyCost * monthsNumbers;
-let discount = 0;
-let wantsPt = confirm("Do you want personal training?");
-let PersonalTrainingCost = 200;
+let itemPrice = +prompt("enter item price?");
+let quantity = +prompt("Enter quantity?");
+let coupon = +prompt("Enter coupon discount % (لو مفيش 0)");
+
+let finalPrice = itemPrice * quantity;
+let discount = finalPrice * (coupon / 100);
 let vat = 0;
 
-if (monthsNumbers >= 6 && monthsNumbers < 12) {
-  discount = (10 / 100) * finalPrice;
-} else if (monthsNumbers >= 12) {
-  discount = (15 / 100) * finalPrice;
-}
 finalPrice = finalPrice - discount;
-
-if (wantsPt == true) {
-  finalPrice = finalPrice + PersonalTrainingCost;
+if (finalPrice >= 1000) {
+  shipping = 0;
+} else {
+  shipping = 60;
 }
+finalPrice = finalPrice + shipping;
+vat = (14 / 100) * finalPrice;
+finalPrice = finalPrice + vat;
 
-vat = 0.14 * finalPrice;
-
-console.log("Discount:", discount);
-console.log("Vat:", vat);
-console.log("Final price:", finalPrice);
+console.log("Item Price:" + itemPrice);
+console.log("Quantity:" + quantity);
+console.log("Discount:" + discount);
+console.log("Vat:" + vat);
+console.log("Final price:" + finalPrice);
